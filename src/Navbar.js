d69af57991aa04a7801ccc24d43804d2cfe555d5
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-03-06 18:16:33 +0300

    fix styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (  
-        <nav className="navbar">
-            <h1>Content<span>Verse</span></h1>
-            <div className="links">
-                <Link to="/" style={{color: "white",
-                    backgroundColor: "#007fff",
-                    borderRadius: "5px"}}>Home</Link>
-                <Link to="/create" style={{
-                    color: "white",
-                    backgroundColor: "#007fff",
-                    borderRadius: "5px"
-                }}>New Blog</Link>
-            </div>
-        </nav>
-    );
-}
- 
+  return (
+    <nav className="flex justify-between items-center my-2 w-full md:w-[40%] mx-auto text-xl font-semibold px-5">
+      <h1 className="text-blue-500">
+        Content<span>Verse</span>
+      </h1>
+      <ul className="flex gap-5">
+        <li className="bg-blue-500 text-white rounded-[10px] px-5 py-2 ">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="bg-blue-500 text-white rounded-[10px] px-5 py-2 flex-shrink-0">
+          <Link to="/create">New Blog</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
 export default Navbar;
