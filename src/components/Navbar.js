b1ca4bdbcb901b1ed4d7af06f942b1ea9fe140c2
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-03-07 10:28:53 +0300

    create file structure for the blog

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [login, setLogin] = useState(false);
   return (
     <nav className="flex justify-between items-center my-2 w-full md:w-[40%] mx-auto text-xl font-semibold px-5">
       <h1 className="text-blue-500">
@@ -10,9 +12,19 @@ const Navbar = () => {
         <li className="bg-blue-500 text-white rounded-[10px] px-5 py-2 ">
           <Link to="/">Home</Link>
         </li>
-        <li className="bg-blue-500 text-white rounded-[10px] px-5 py-2 flex-shrink-0">
-          <Link to="/create">New Blog</Link>
-        </li>
+        {login ? (
+          <li className="bg-blue-500 text-white rounded-[10px] px-5 py-2 flex-shrink-0">
+            <Link to="/create">New Blog</Link>
+          </li>
+        ) : (
+          ""
+        )}
+        <button
+          onClick={() => setLogin(!login)}
+          className="bg-blue-500 text-white rounded-[10px] px-5 py-2 flex-shrink-0"
+        >
+          Login
+        </button>
       </ul>
     </nav>
   );
